b74c703515563b48dabd23495abd580775cbfd10
Chee Kian Teoh <teoh4770@mylaurier.ca>
2026-06-09 23:24:11 +0800

    fix prompt seeder property

--- database/seeders/PromptSeeder.php
+++ database/seeders/PromptSeeder.php
@@ -14,14 +14,14 @@ class PromptSeeder extends Seeder
     public function run(): void
     {
         $prompts = [
-            ['ritual' => 'interrupt', 'question' => 'What are you avoiding right now that you know would move the needle?'],
-            ['ritual' => 'interrupt', 'question' => 'Is your current activity a distraction or a priority? Be honest.'],
-            ['ritual' => 'interrupt', 'question' => 'If you died tonight, would you be proud of how you spent this last hour?'],
-            ['ritual' => 'interrupt', 'question' => 'What "forbidden action" are you justifying to yourself at this moment?'],
-            ['ritual' => 'interrupt', 'question' => 'Who are you trying to impress with your current "busyness"?'],
-            ['ritual' => 'interrupt', 'question' => 'Stop. Look around. What in your environment is tolerating mediocrity?'],
-            ['ritual' => 'interrupt', 'question' => 'If your future self saw you right now, would they be disgusted or inspired?'],
-            ['ritual' => 'interrupt', 'question' => 'What is the most uncomfortable truth you are currently ignoring?'],
+            ['ritual' => 'interrupt', 'body' => 'What are you avoiding right now that you know would move the needle?'],
+            ['ritual' => 'interrupt', 'body' => 'Is your current activity a distraction or a priority? Be honest.'],
+            ['ritual' => 'interrupt', 'body' => 'If you died tonight, would you be proud of how you spent this last hour?'],
+            ['ritual' => 'interrupt', 'body' => 'What "forbidden action" are you justifying to yourself at this moment?'],
+            ['ritual' => 'interrupt', 'body' => 'Who are you trying to impress with your current "busyness"?'],
+            ['ritual' => 'interrupt', 'body' => 'Stop. Look around. What in your environment is tolerating mediocrity?'],
+            ['ritual' => 'interrupt', 'body' => 'If your future self saw you right now, would they be disgusted or inspired?'],
+            ['ritual' => 'interrupt', 'body' => 'What is the most uncomfortable truth you are currently ignoring?'],
         ];
 
         foreach ($prompts as $prompt) {
