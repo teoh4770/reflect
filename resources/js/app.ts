121d9e08b2b6c655408f0714b0324f10c10f9cef
Chee Kian Teoh <teoh4770@mylaurier.ca>
2026-06-10 13:14:20 +0800

    doing setup for echo, pusher and reverb

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,13 +1,9 @@
 import '../css/app.css';
+import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, DefineComponent, h } from 'vue';
-import { configureEcho } from '@laravel/echo-vue';
-
-configureEcho({
-    broadcaster: 'reverb',
-});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
