a239032d9095113a638dc223d4a4cf5d8eb5d7c8
Chee Kian Teoh <teoh4770@mylaurier.ca>
2026-06-15 16:16:03 +0800

    add remaining required files for pwa and push notification

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -8,6 +8,18 @@ import { createApp, h } from 'vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
+if ('serviceWorker' in navigator) {
+    const params = new URLSearchParams({
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    });
+    navigator.serviceWorker.register('/firebase-messaging-sw.js?' + params.toString());
+}
+
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     resolve: (name) =>
