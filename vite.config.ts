0a6098bfc50c073c7970b86a7fcf329e52ee4b4e
Chee Kian Teoh <teoh4770@mylaurier.ca>
2026-06-15 15:04:17 +0800

    connect firebase for push notification and setup manifest in app.blade.php

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
@@ -29,6 +30,52 @@ export default defineConfig({
         }),
         wayfinder({
             formVariants: true,
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            outDir: 'public',
+            buildBase: '/',
+            scope: '/',
+            injectRegister: null,
+            workbox: {
+                navigateFallback: null,
+                cleanupOutdatedCaches: true,
+                globPatterns: ['**/*.{js,css,woff2,woff}'],
+                manifestTransforms: [
+                    async (entries) => {
+                        return {
+                            manifest: entries.filter(e => !e.url.endsWith('manifest.webmanifest')),
+                            warnings: []
+                        };
+                    }
+                ]
+            },
+            manifest: {
+                name: 'Reflect',
+                short_name: 'Reflect',
+                description: 'Reflect App',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                display: 'standalone',
+                start_url: '/',
+                icons: [
+                    {
+                        src: '/apple-touch-icon.png',
+                        sizes: '180x180',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/apple-touch-icon.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/apple-touch-icon.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            },
         }),
     ],
 });
