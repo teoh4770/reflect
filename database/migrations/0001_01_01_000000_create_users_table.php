a680db2f233af64eb08bebc10ac5b33530a696bf
Chee Kian Teoh <teoh4770@mylaurier.ca>
2026-06-15 17:46:30 +0800

    feat: extract fcm_token into dedicated migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->text('identity_statement')->nullable();
-            $table->string('fcm_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
