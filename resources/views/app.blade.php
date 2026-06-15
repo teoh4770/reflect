86ca89277f2d058afe135144ca00acb5a1dfd146
Chee Kian Teoh <teoh4770@mylaurier.ca>
2026-06-15 13:02:53 +0800

    Revert "add android asset + improve navigation offset"
    This reverts commit 2b943838f41ba7dc1c690992f7e07730d3352e00.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"  @class(['dark' => ($appearance ?? 'system') == 'dark'])>
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
