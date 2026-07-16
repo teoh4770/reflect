342551ec0e582a5d7347462a1b0870fc3a5c5413
Chee Kian Teoh <teoh4770@mylaurier.ca>
2026-07-16 14:11:02 +0800

    feat: setup feedback controller to only have store methods

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -5,35 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Requests\FeedbackRequest;
 use App\Http\Resources\FeedbackResource;
 use App\Models\Feedback;
+use Symfony\Component\HttpFoundation\Response;
 
 class FeedbackController extends Controller
 {
-	public function index()
-	{
-		return FeedbackResource::collection(Feedback::all());
-	}
+    public function store(FeedbackRequest $request)
+    {
+        $createdFeedback = Feedback::query()->create($request->validated());
+        $feedback = new FeedbackResource($createdFeedback);
 
-	public function store(FeedbackRequest $request)
-	{
-		return new FeedbackResource(Feedback::create($request->validated()));
-	}
-
-	public function show(Feedback $feedback)
-	{
-		return new FeedbackResource($feedback);
-	}
-
-	public function update(FeedbackRequest $request, Feedback $feedback)
-	{
-		$feedback->update($request->validated());
-
-		return new FeedbackResource($feedback);
-	}
-
-	public function destroy(Feedback $feedback)
-	{
-		$feedback->delete();
-
-		return response()->json();
-	}
+        return response()->json($feedback, Response::HTTP_CREATED);
+    }
 }
